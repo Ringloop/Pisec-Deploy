728c0333c5276554064009ac8ddf323d3344bc2a
ringloop <ringloop@_HOSTNAME_>
2022-05-29 11:49:53 +0200

    fix deployment

--- deploy-crawler.sh
+++ deploy-crawler.sh
@@ -1,3 +1,3 @@
-sed -i 's;.*pisec-brain.*$;    image: ringloop/pisec-crawler:'"$1"';' docker-compose.yml
+sed -i 's;.*pisec-crawler.*$;    image: ringloop/pisec-crawler:'"$1"';' docker-compose.yml
 docker-compose up -d crawler
 
